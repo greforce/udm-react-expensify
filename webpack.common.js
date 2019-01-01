c8de59573126d0da84e22d2ca78504c2bcab322a
greforce <greforce76@gmail.com>
2019-01-01 13:15:02 +0300

    correction in setup test database env

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,17 @@
 const path = require('path');
+const webpack = require('webpack');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'test') {
+  console.log('setting test')
+  require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+  console.log('setting development')
+  require('dotenv').config({ path: '.env.development' });
+} else {
+  console.log(process.env.NODE_ENV);
+}
 
 module.exports = {
   entry: './src/app.js',
@@ -6,6 +19,16 @@ module.exports = {
     path: path.join(__dirname, 'public', 'dist'),
     filename: 'bundle.js',
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+      'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+      'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+      'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+      'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+    }),
+  ],
   module: {
     rules: [{
       test: /\.js$/,
